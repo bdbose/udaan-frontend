6e35aed5f7e9dd78df7d13313c43a6ce62275cad
bdbose <bdbose123@gmail.com>
2021-09-14 11:38:48 +0530

    logo update

--- new/src/App.js
+++ new/src/App.js
@@ -20,10 +20,11 @@ function App() {
               {/* Brand and toggle get grouped for better mobile display */}
               {/* <a href="index.html" class="navbar-brand"><img src="assets/img/logo.png" alt=""></a> */}
               <img
-                src='/assets/img/logo1.png'
+                src='/assets/img/logo.png'
                 style={{
-                  width: '50px',
-                  height: '50px',
+                  width: '100px',
+                  height: '100px',
+                  margin: '10px 0',
                 }}
                 alt='Logo'
               />
